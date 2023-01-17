d5971be0b8d2fc57dd139685c3d3eb5760b9c774
Ureakim <dev@ureakim.net>
2023-01-17 23:43:39 +0100

    add lualine

--- lua/ureakim/plugins.lua
+++ lua/ureakim/plugins.lua
@@ -1,21 +1,23 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	{"nvim-lualine/lualine.nvim"},
+
 	{"nvim-treesitter/nvim-treesitter", init = TSUpdate},
 
 	{"nvim-telescope/telescope.nvim", tag = "0.1.0",
-		dependencies = {"nvim-lua/plenary.nvim"}},
+	dependencies = {"nvim-lua/plenary.nvim"}},
 
 	{"neovim/nvim-lspconfig"},
 
