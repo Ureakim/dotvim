c6000288dafa16a4dad137e5f38f9e5b31ed0ba3
Ureakim <dev@ureakim.net>
2023-01-18 22:47:46 +0100

    fix remaps

--- lua/ureakim/remaps.lua
+++ lua/ureakim/remaps.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = " "               -- space as leader key
 
-vim.keymap.set('i', '<C-l>', '<C-x><C-o>')   -- autocomplete with ctrl + l
+vim.keymap.set('i', '<C-l>', '<C-x><C-o>', { noremap = true})   -- autocomplete with ctrl + l
 
 -- splits
-vim.keymap.set('n', '<leader>sh', '<C-w>h')
-vim.keymap.set('n', '<leader>sj', '<C-w>j')
-vim.keymap.set('n', '<leader>sk', '<C-w>k')
-vim.keymap.set('n', '<leader>sl', '<C-w>l')
+vim.keymap.set('n', '<leader>sh', '<C-w>h', { noremap = true})
+vim.keymap.set('n', '<leader>sj', '<C-w>j', { noremap = true})
+vim.keymap.set('n', '<leader>sk', '<C-w>k', { noremap = true})
+vim.keymap.set('n', '<leader>sl', '<C-w>l', { noremap = true})
