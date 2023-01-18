4cba1016e8435e6fed5d9200b8fde0dceea2a420
Ureakim <dev@ureakim.net>
2023-01-18 22:52:15 +0100

    add netrw remap

--- lua/ureakim/remaps.lua
+++ lua/ureakim/remaps.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "               -- space as leader key
 
 vim.keymap.set('i', '<C-l>', '<C-x><C-o>', { noremap = true})   -- autocomplete with ctrl + l
 
+vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)   -- autocomplete with ctrl + l
+
 -- splits
 vim.keymap.set('n', '<leader>sh', '<C-w>h', { noremap = true})
 vim.keymap.set('n', '<leader>sj', '<C-w>j', { noremap = true})
