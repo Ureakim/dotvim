8a266fa95211d837a71f424750f5038b47fa87ef
Ureakim <dev@ureakim.net>
2023-01-19 11:05:16 +0100

    change split remaps

--- lua/ureakim/remaps.lua
+++ lua/ureakim/remaps.lua
@@ -4,8 +4,4 @@ vim.keymap.set('i', '<C-l>', '<C-x><C-o>', { noremap = true})   -- autocomplete 
 
 vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)   -- autocomplete with ctrl + l
 
--- splits
-vim.keymap.set('n', '<leader>sh', '<C-w>h', { noremap = true})
-vim.keymap.set('n', '<leader>sj', '<C-w>j', { noremap = true})
-vim.keymap.set('n', '<leader>sk', '<C-w>k', { noremap = true})
-vim.keymap.set('n', '<leader>sl', '<C-w>l', { noremap = true})
+vim.keymap.set('n', '<C-e>', '<C-w>', { noremap = true})
