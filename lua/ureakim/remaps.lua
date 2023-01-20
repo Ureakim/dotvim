14cc2b618fa70bb3b7a4d3a46cc2f7afd10de7d4
Ureakim <dev@ureakim.net>
2023-01-20 15:45:19 +0100

    remove useless remap

--- lua/ureakim/remaps.lua
+++ lua/ureakim/remaps.lua
@@ -3,5 +3,3 @@ vim.g.mapleader = " "               -- space as leader key
 vim.keymap.set('i', '<C-l>', '<C-x><C-o>', { noremap = true})   -- autocomplete with ctrl + l
 
 vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)   -- autocomplete with ctrl + l
-
-vim.keymap.set('n', '<C-e>', '<C-w>', { noremap = true})
