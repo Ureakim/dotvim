065e3e73408536dbfa2b4643ec62a8436d334e74
Ureakim <dev@ureakim.net>
2023-01-23 10:11:24 +0100

    add run project

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -3,3 +3,4 @@ vim.g.rust_recommended_style = false   -- do not use the default formatting styl
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>pt', ':terminal cargo test<cr>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>pb', ':terminal cargo build<cr>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>pc', ':terminal cargo check<cr>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>pr', ':terminal cargo run<cr>', { noremap = true })
