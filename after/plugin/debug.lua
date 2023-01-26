674c0386697455696d99acdf0b14d25023c3d9c1
Ureakim <dev@ureakim.net>
2023-01-26 12:25:57 +0100

    autocomplete rust bins for debugging

--- after/plugin/debug.lua
+++ after/plugin/debug.lua
@@ -1,12 +1,48 @@
 local dap = require('dap')
+local dapui = require("dapui")
+
 require('dapui').setup({})
 
--- cargo run --package
-function _G.getRustPackages()
-	return 'test\ntest2\ntest3\ntrust'
+-- get all the bin available in a rust project
+function _G.UreakimGetRustBins()
+	local cargoResult = vim.fn.system("cargo run --bin")
+
+	-- split result by lines
+	local lines = {}
+	for line in cargoResult:gmatch("[^\n]+") do
+		table.insert(lines, line)
+	end
+
+	-- get only the bins and clean them
+	local bins = {}
+	for key, line in pairs(lines) do
+		if key > 2 then
+			local bin = string.gsub(line, "%s+", "")
+			table.insert(bins, bin)
+		end
+	end
+
+	return bins
 end
 
-local cwd = vim.fn.getcwd() .. '/target/debug/'
+-- convert the bin list in a completion string
+function _G.UreakimGetRustBinsCompletion()
+	local bins = _G.UreakimGetRustBins()
+	return table.concat(bins, "\n")
+end
+
+local function getRustBins()
+	local bins = _G.UreakimGetRustBins()
+
+	if #bins == 1 then
+		vim.fn.system("cargo build")
+		return vim.fn.getcwd() .. '/target/debug/' .. bins[1]
+	end
+
+	local choice = vim.fn.input('Bin to debug > ', '', 'custom,v:lua.UreakimGetRustBinsCompletion')
+	vim.fn.system("cargo build")
+	return vim.fn.getcwd() .. '/target/debug/' .. choice
+end
 
 dap.adapters.codelldb = {
 	type = 'server',
@@ -23,13 +59,7 @@ dap.configurations.rust = {
 		type = 'codelldb',
 		cwd = '${workspaceFolder}',
 		request = 'launch',
-		program = function()
-			return vim.fn.input({
-				prompt = 'Path to executable: ',
-		--		default = cwd,
-				completion = "custom,v:lua.getRustPackages",
-			})
-		end,
+		program = getRustBins,
 		stopOnEntry = false,
 		terminal = 'integrated',
 		sourceLanguages = { 'rust' },
@@ -37,7 +67,6 @@ dap.configurations.rust = {
 	},
 }
 
-local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
 end
