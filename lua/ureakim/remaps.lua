33d5ffeadeca3ed1d6e4fc28efb7c18fe3a26e6b
Ureakim <dev@ureakim.net>
2023-02-12 23:10:24 +0100

    Ex mode ? no thx.

--- lua/ureakim/remaps.lua
+++ lua/ureakim/remaps.lua
@@ -3,3 +3,5 @@ vim.g.mapleader = " "               -- space as leader key
 vim.keymap.set('i', '<C-l>', '<C-x><C-o>', { noremap = true})   -- autocomplete with ctrl + l
 
 vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)   -- autocomplete with ctrl + l
+
+vim.keymap.set('n', 'Q', "<nop>")   -- Leave me alone Ex mode !
