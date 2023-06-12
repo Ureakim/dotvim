96ca1e109e100d3e040ada5c40cd4b967e9eaa68
Ureakim <dev@ureakim.net>
2023-06-12 09:28:55 +0200

    fix fetching tags

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 cd nvim-src
-git checkout stable
+git checkout master
 git pull
+git fetch --tags --force
+git checkout stable
 make distclean
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
