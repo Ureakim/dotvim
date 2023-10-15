1ab6e5a040f69b817ae3dcbcf4b5b5d85e099f0d
Ureakim <dev@ureakim.net>
2023-10-15 16:33:22 +0200

    fix autocmd

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ git fetch --tags --force
 git checkout stable
 make distclean
 make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo rm /usr/local/bin/nvim
+sudo rm -r /usr/local/share/nvim/
 sudo make install
--- lua/ureakim/autocmd.lua
+++ lua/ureakim/autocmd.lua
@@ -1,7 +1,7 @@
 local group = vim.api.nvim_create_augroup("UreakimAugroup", {clear = true})
 
 -- close preview split after completion
-vim.api.nvim_create_autocmd("InsertLeave,CompleteDone", {
+vim.api.nvim_create_autocmd({"InsertLeave", "CompleteDone"}, {
 	callback = function()
 		if vim.fn.pumvisible() == 0 then
 			vim.cmd("pclose")
