3cfd67f37dc2888c00e0f4aba84df320902593a8
Ureakim <dev@ureakim.net>
2023-11-12 18:16:46 +0100

    add ftplugin for lua

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+vim.opt.tabstop = 4        -- how many <space> per <tab>
+vim.opt.softtabstop = 4    -- how many <space> deleted when backspacing
+vim.opt.shiftwidth = 4     -- how many <space> per indent command
+vim.opt.expandtab = true   -- replace <tab> with <space>
