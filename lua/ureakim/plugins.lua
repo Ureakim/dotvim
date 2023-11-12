b14f34836d3fd8cb0b7b9f69e94e0a36777f02a5
Ureakim <dev@ureakim.net>
2023-11-12 19:58:40 +0100

    add copilot

--- lua/ureakim/plugins.lua
+++ lua/ureakim/plugins.lua
@@ -1,36 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ "Ureakim/nebulae.nvim",            lazy = false,         priority = 1000 },
-	{ "nvim-treesitter/nvim-treesitter", init = TSUpdate },
-	{ "neovim/nvim-lspconfig" },
-	{ "windwp/nvim-autopairs",           event = "InsertEnter" },
-	{
-		"mfussenegger/nvim-dap"
-		,
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-		}
-	},
-	{ "alexghergh/nvim-tmux-navigation" },
+    { "Ureakim/nebulae.nvim",             lazy = false,                              priority = 1000 },
+    { "nvim-treesitter/nvim-treesitter",  init = TSUpdate },
+    { "zbirenbaum/copilot.lua",           event = "InsertEnter",                     cmd = "Copilot" },
+    { "neovim/nvim-lspconfig" },
+    { "windwp/nvim-autopairs",            event = "InsertEnter" },
+    { "mfussenegger/nvim-dap",            dependencies = { "rcarriga/nvim-dap-ui", } },
+    { "alexghergh/nvim-tmux-navigation" },
 
-	{ "j-hui/fidget.nvim",                tag = "legacy" },
-	{ "nvim-lualine/lualine.nvim" },
+    { "j-hui/fidget.nvim",                tag = "legacy" },
+    { "nvim-lualine/lualine.nvim" },
 
-	{ "dcampos/nvim-snippy" },
+    { "dcampos/nvim-snippy" },
 
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ "ibhagwan/fzf-lua" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "ibhagwan/fzf-lua" },
 })
