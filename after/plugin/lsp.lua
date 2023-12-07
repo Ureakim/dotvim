0bc372aeeab43b9578915537073b3767822934c6
Ureakim <dev@ureakim.net>
2023-12-07 22:37:32 +0100

    add c/c++ lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,6 @@
 -- LSP Config
 vim.diagnostic.config({
-	virtual_text = false -- deactivate virtual text
+    virtual_text = false -- deactivate virtual text
 })
 
 -- LSP Mappings
@@ -14,24 +14,24 @@ vim.keymap.set('n', '<leader>gl', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, bufopts)
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	vim.keymap.set('n', '<leader>lk', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<leader>lD', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
-	vim.keymap.set('n', '<leader>lR', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<leader>lk', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>lD', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
+    vim.keymap.set('n', '<leader>lR', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
 end
 
 local lsp_flags = {
-	-- This is the default in Nvim 0.7+
-	debounce_text_changes = 150,
+    -- This is the default in Nvim 0.7+
+    debounce_text_changes = 150,
 }
 
 --
@@ -40,35 +40,40 @@ local lsp_flags = {
 
 -- Rust
 require('lspconfig')['rust_analyzer'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	['rust-analyzer'] = {
-		diagnostics = {
-			enable = false,
-		}
-	}
+    on_attach = on_attach,
+    flags = lsp_flags,
+    ['rust-analyzer'] = {
+        diagnostics = {
+            enable = false,
+        }
+    }
 }
 
 -- LUA
 require('lspconfig')['lua_ls'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	settings = {
-		Lua = {
-			-- Version of Lua used
-			runtime = { version = 'LuaJIT' },
-			-- Get the language server to recognize the `vim` global
-			diagnostics = { globals = { 'vim' } },
-			-- Make the server aware of Neovim runtime files
-			workspace = {
-				library = {
-					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
-					[vim.fn.stdpath('config') .. '/lua'] = true,
-				},
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = { enable = false },
-		},
-	}
+    on_attach = on_attach,
+    flags = lsp_flags,
+    settings = {
+        Lua = {
+            -- Version of Lua used
+            runtime = { version = 'LuaJIT' },
+            -- Get the language server to recognize the `vim` global
+            diagnostics = { globals = { 'vim' } },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.stdpath('config') .. '/lua'] = true,
+                },
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = { enable = false },
+        },
+    }
 }
 
+-- C/C++
+require('lspconfig')['clangd'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
