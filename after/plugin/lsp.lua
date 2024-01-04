8bd9b24a1529fe178e1a2cec89099b3e5fbf61d3
Ureakim <dev@ureakim.net>
2024-01-04 14:17:58 +0100

    add csharp lsp with omnisharp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -77,3 +77,17 @@ require('lspconfig')['clangd'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
 }
+
+-- C#
+require('lspconfig')['omnisharp'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+    cmd = { vim.fn.expand('$HOME/.local/share/nvim/mason/bin/omnisharp') },
+    enable_editorconfig_support = true,
+    enable_ms_build_load_projects_on_demand = false,
+    enable_roslyn_analyzers = false,
+    organize_imports_on_format = false,
+    enable_import_completion = false,
+    sdk_include_prereleases = true,
+    analyze_open_documents_only = false,
+}
