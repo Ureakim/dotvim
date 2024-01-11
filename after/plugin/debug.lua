51ce00837c1ccc9a84e97626a7d9f74decde9277
Ureakim <dev@ureakim.net>
2024-01-11 14:03:05 +0100

    add dap csharp

--- after/plugin/debug.lua
+++ after/plugin/debug.lua
@@ -3,76 +3,101 @@ local dapui = require("dapui")
 
 require('dapui').setup({})
 
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
+
+--
+-- Rust
+--
+
 -- get all the bin available in a rust project
 function _G.UreakimGetRustBins()
-	local cargoResult = vim.fn.system("cargo run --bin")
+    local cargoResult = vim.fn.system("cargo run --bin")
 
-	-- split result by lines
-	local lines = {}
-	for line in cargoResult:gmatch("[^\n]+") do
-		table.insert(lines, line)
-	end
+    -- split result by lines
+    local lines = {}
+    for line in cargoResult:gmatch("[^\n]+") do
+        table.insert(lines, line)
+    end
 
-	-- get only the bins and clean them
-	local bins = {}
-	for key, line in pairs(lines) do
-		if key > 2 then
-			local bin = string.gsub(line, "%s+", "")
-			table.insert(bins, bin)
-		end
-	end
+    -- get only the bins and clean them
+    local bins = {}
+    for key, line in pairs(lines) do
+        if key > 2 then
+            local bin = string.gsub(line, "%s+", "")
+            table.insert(bins, bin)
+        end
+    end
 
-	return bins
+    return bins
 end
 
 -- convert the bin list in a completion string
 function _G.UreakimGetRustBinsCompletion()
-	local bins = _G.UreakimGetRustBins()
-	return table.concat(bins, "\n")
+    local bins = _G.UreakimGetRustBins()
+    return table.concat(bins, "\n")
 end
 
 local function getRustBins()
-	local bins = _G.UreakimGetRustBins()
+    local bins = _G.UreakimGetRustBins()
 
-	if #bins == 1 then
-		vim.fn.system("cargo build")
-		return vim.fn.getcwd() .. '/target/debug/' .. bins[1]
-	end
+    if #bins == 1 then
+        vim.fn.system("cargo build")
+        return vim.fn.getcwd() .. '/target/debug/' .. bins[1]
+    end
 
-	local choice = vim.fn.input('Bin to debug > ', '', 'custom,v:lua.UreakimGetRustBinsCompletion')
-	vim.fn.system("cargo build")
-	return vim.fn.getcwd() .. '/target/debug/' .. choice
+    local choice = vim.fn.input('Bin to debug > ', '', 'custom,v:lua.UreakimGetRustBinsCompletion')
+    vim.fn.system("cargo build")
+    return vim.fn.getcwd() .. '/target/debug/' .. choice
 end
 
 dap.adapters.codelldb = {
-	type = 'server',
-	port =  13000,
-	executable = {
-		command = vim.fn.stdpath("data") .. '/mason/bin/codelldb',
-		args = {"--port", "13000"},
-	}
+    type = 'server',
+    port = 13000,
+    executable = {
+        command = vim.fn.stdpath("data") .. '/mason/bin/codelldb',
+        args = { "--port", "13000" },
+    }
 }
 
 dap.configurations.rust = {
-	{
-		name = 'Launch',
-		type = 'codelldb',
-		cwd = '${workspaceFolder}',
-		request = 'launch',
-		program = getRustBins,
-		stopOnEntry = false,
-		terminal = 'integrated',
-		sourceLanguages = { 'rust' },
-		showDisassembly = "never",
-	},
+    {
+        name = 'Launch',
+        type = 'codelldb',
+        cwd = '${workspaceFolder}',
+        request = 'launch',
+        program = getRustBins,
+        stopOnEntry = false,
+        terminal = 'integrated',
+        sourceLanguages = { 'rust' },
+        showDisassembly = "never",
+    },
 }
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
-	dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-	dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-	dapui.close()
-end
+--
+-- Dotnet
+--
+
+dap.adapters.coreclr = {
+    type = 'executable',
+    command = vim.fn.stdpath('data') .. '/mason/bin/netcoredbg',
+    args = { '--interpreter=vscode' }
+}
+
+dap.configurations.cs = {
+    {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        end,
+    },
+}
