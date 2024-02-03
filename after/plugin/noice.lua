ca0c63d5103b653ddd8b0f95787fc4a99e280887
Ureakim <dev@ureakim.net>
2024-02-03 17:40:03 +0100

    disable messages notif

--- after/plugin/noice.lua
+++ after/plugin/noice.lua
@@ -4,7 +4,7 @@ require("noice").setup({
         override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+            ["cmp.entry.get_documentation"] = false, -- requires hrsh7th/nvim-cmp
         },
     },
     -- you can enable a preset for easier configuration
@@ -14,5 +14,8 @@ require("noice").setup({
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false,       -- add a border to hover docs and signature help
+    },
+    messages = {
+        view = false,       -- disable notification for messages
     },
 })
