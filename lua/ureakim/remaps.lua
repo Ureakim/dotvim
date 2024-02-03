9f40d48c97e05fe4e9775dab678ffbebcc03d8ac
Ureakim <dev@ureakim.net>
2024-02-03 19:06:47 +0100

    fix term mode keymap

--- lua/ureakim/remaps.lua
+++ lua/ureakim/remaps.lua
@@ -1,10 +1,11 @@
 vim.g.mapleader = " "               -- space as leader key
 
-vim.keymap.set('i', '<C-l>', '<C-x><C-o>', { noremap = true})   -- autocomplete with ctrl + l
+vim.keymap.set('i', '<C-l>', '<C-x><C-o>', { noremap = true })   -- autocomplete with ctrl + l
 
-vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)   -- autocomplete with ctrl + l
-
+vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)   -- Open Netrw file manager
 vim.keymap.set('n', 'Q', "<nop>")   -- Leave me alone Ex mode !
+
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })  -- Disable insert mode in term with Escape
 
 -- center cursor after half page motion
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
