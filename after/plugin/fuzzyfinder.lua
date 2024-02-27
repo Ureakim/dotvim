97df5107d7918351b7a9fef877a283f00f1472a4
Ureakim <dev@ureakim.net>
2024-02-27 11:25:57 +0100

    add keybind document symbols

--- after/plugin/fuzzyfinder.lua
+++ after/plugin/fuzzyfinder.lua
@@ -5,4 +5,5 @@ vim.keymap.set('n', '<leader>fb', fzf.buffers, {})
 vim.keymap.set('n', '<leader>lr', fzf.lsp_references, {})
 vim.keymap.set('n', '<leader>li', fzf.lsp_implementations, {})
 vim.keymap.set('n', '<leader>la', fzf.lsp_code_actions, {})
+vim.keymap.set('n', '<leader>ls', fzf.lsp_document_symbols, {})
 
