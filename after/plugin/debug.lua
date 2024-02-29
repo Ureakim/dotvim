200289c9f010543f70b38771d1101d8ce1afd216
Ureakim <dev@ureakim.net>
2024-02-29 14:42:49 +0100

    add delve for golang debug

--- after/plugin/debug.lua
+++ after/plugin/debug.lua
@@ -1,6 +1,20 @@
 local dap = require('dap')
 local dapui = require("dapui")
 
+--
+-- Keymaps
+--
+
+vim.keymap.set('n', '<leader>dd', dap.continue)
+vim.keymap.set('n', '<leader>dc', dap.terminate)
+vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint)
+vim.keymap.set('n', '<leader>di', dap.step_into)
+vim.keymap.set('n', '<leader>do', dap.step_over)
+
+--
+-- DAP Config
+--
+
 require('dapui').setup({})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
@@ -100,4 +114,56 @@ dap.configurations.cs = {
             return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
         end,
     },
+}
+
+--
+-- Golang
+--
+
+local function getGoProjectRoot()
+    local gomod = vim.fn.system("go env GOMOD")
+    local path = gomod:match("(.*[/\\])")
+    local mainfile = path .. "main.go"
+    return mainfile
+end
+
+dap.adapters.delve = {
+    type = 'server',
+    port = '${port}',
+    executable = {
+        command = 'dlv',
+        args = { 'dap', '-l', '127.0.0.1:${port}' },
+    }
+}
+
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+    {
+        type = "delve",
+        name = "Debug",
+        request = "launch",
+        program = getGoProjectRoot,
+        args = function()
+            -- ask user for the arguments
+            local args
+            vim.ui.input({ prompt = "Arguments: " },
+                function(str) args = str or "" end)
+            return vim.split(args, " ")
+        end
+    },
+    {
+        type = "delve",
+        name = "Debug test", -- configuration for debugging test files
+        request = "launch",
+        mode = "test",
+        program = getGoProjectRoot,
+    },
+    -- works with go.mod packages and sub packages
+    -- {
+    --  type = "delve",
+    --  name = "Debug test (go.mod)",
+    --  request = "launch",
+    --  mode = "test",
+    --  program = "./${relativeFileDirname}"
+    -- }
 }
