62dddd671f3c125c2019507d327b9a899b71df26
Ureakim <dev@ureakim.net>
2024-10-31 11:16:34 +0100

    add mini.comment and mini.hipatterns

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -1,6 +1,16 @@
 require('mini.icons').setup()
 require('mini.notify').setup()
 require('mini.pairs').setup()
+require('mini.comment').setup()
+require('mini.hipatterns').setup({
+    highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'SpellBad' },
+        hack  = { pattern = '%f[%w]()HACK()%f[%W]', group = 'SpellBad' },
+        todo  = { pattern = '%f[%w]()TODO()%f[%W]', group = 'SpellBad' },
+        note  = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'SpellCap' },
+    }
+})
 require('mini.completion').setup({
     window = {
         info = { border = 'single' },
