a241367d4a403810f5fe5052939660131a68ca28
Ureakim <dev@ureakim.net>
2024-11-03 23:04:19 +0100

    fix formatting

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -23,10 +23,9 @@ require("CopilotChat").setup {
     debug = false,
 }
 
-
 vim.keymap.set('n', '<leader>bc', '<CMD>CopilotChatToggle<CR>', { desc = "Toggle Copilot Chat" })
 vim.keymap.set({ 'n', 'v' }, '<leader>be', '<CMD>CopilotChatExplain<CR>', { desc = "Explain selected code" })
 vim.keymap.set({ 'n', 'v' }, '<leader>bf', '<CMD>CopilotChatFix<CR>', { desc = "Fix selected code" })
 vim.keymap.set({ 'n', 'v' }, '<leader>bd', '<CMD>CopilotChatDocs<CR>', { desc = "Add docs to selected code" })
 vim.keymap.set({ 'n', 'v' }, '<leader>bt', '<CMD>CopilotChatTests<CR>', { desc = "Create tests for selected code" })
- 
+
