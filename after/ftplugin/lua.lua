af438d6b97b39b6459eb78bed158f774716b5752
Ureakim <dev@ureakim.net>
2024-11-25 10:47:39 +0100

    fix vim.opt by vim.opt_local in ftplugins

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,4 +1,4 @@
-vim.opt.tabstop = 4        -- how many <space> per <tab>
-vim.opt.softtabstop = 4    -- how many <space> deleted when backspacing
-vim.opt.shiftwidth = 4     -- how many <space> per indent command
-vim.opt.expandtab = true   -- replace <tab> with <space>
+vim.opt_local.tabstop = 4        -- how many <space> per <tab>
+vim.opt_local.softtabstop = 4    -- how many <space> deleted when backspacing
+vim.opt_local.shiftwidth = 4     -- how many <space> per indent command
+vim.opt_local.expandtab = true   -- replace <tab> with <space>
