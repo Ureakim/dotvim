71f59e2f643a60b9aead6e9ee7fca9530a048fc7
Ureakim <dev@ureakim.net>
2024-12-21 22:22:54 +0100

    edit yaml config

--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -1,4 +1,4 @@
-vim.opt.tabstop = 4        -- how many <space> per <tab>
-vim.opt.softtabstop = 4    -- how many <space> deleted when backspacing
-vim.opt.shiftwidth = 4     -- how many <space> per indent command
+vim.opt.tabstop = 2        -- how many <space> per <tab>
+vim.opt.softtabstop = 2    -- how many <space> deleted when backspacing
+vim.opt.shiftwidth = 2     -- how many <space> per indent command
 vim.opt.expandtab = true   -- replace <tab> with <space>
