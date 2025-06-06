0917ce5677ad63de8be6a807f12f5c96cca62995
Ureakim <dev@ureakim.net>
2025-06-06 10:10:25 +0200

    add lua debugger

--- after/plugin/debug.lua
+++ after/plugin/debug.lua
@@ -167,3 +167,40 @@ dap.configurations.go = {
     --  program = "./${relativeFileDirname}"
     -- }
 }
+
+--
+-- LUA
+--
+
+dap.adapters["local-lua"] = {
+    type = "executable",
+    command = "node",
+    args = {
+        vim.fn.stdpath('data') .. '/mason/packages/local-lua-debugger-vscode/extension/extension/debugAdapter.js'
+    },
+    enrich_config = function(config, on_config)
+        if not config["extensionPath"] then
+            local c = vim.deepcopy(config)
+            -- 💀 If this is missing or wrong you'll see
+            -- "module 'lldebugger' not found" errors in the dap-repl when trying to launch a debug session
+            c.extensionPath = vim.fn.stdpath('data') .. '/mason/packages/local-lua-debugger-vscode/extension'
+            on_config(c)
+        else
+            on_config(config)
+        end
+    end,
+}
+
+dap.configurations.lua = {
+    {
+        name = 'Current file (local-lua-dbg, lua)',
+        type = 'local-lua',
+        request = 'launch',
+        cwd = '${workspaceFolder}',
+        program = {
+            lua = 'lua5.4',
+            file = '${file}',
+        },
+        args = {},
+    },
+}
